a9f1e5781a2dcef07e356b36cae82a74330983b1
Unknown <ohgead@gmail.com>
2019-03-15 18:04:00 -0400

    Added Project

---
+++ Assets/_Scripts/BackgroundScroller.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundScroller : MonoBehaviour {
+
+	enum Direction {LEFT, RIGHT, UP, DOWN};
+
+	[SerializeField]
+	List<Sprite> backgroundImages;
+	[SerializeField]
+	float scrollSpeed = 5f;
+	[SerializeField]
+	int tileSize = 25;
+	[SerializeField]
+	Direction scrollDirection = Direction.DOWN;
+	[SerializeField]
+	GameObject tilePrefab;
+
+	List<GameObject> backgroundTiles;
+	RectTransform rt;
+
+	// Use this for initialization
+	void Start () {
+		rt = this.GetComponent<RectTransform>();
+		if (backgroundImages != null)
+		{
+			backgroundTiles = new List<GameObject>();
+			GenerateBackground();
+		}
+	}
+
+
+	private void Update()
+	{
+		if (backgroundTiles != null)
+		{
+			ScrollTiles();
+		}
+	}
+
+	void ScrollTiles()
+	{
+		Vector3 scrollOffset = Vector3.zero;
+
+		switch(scrollDirection)
+		{
+			case Direction.LEFT:
+				scrollOffset = new Vector3(-scrollSpeed, 0);
+				break;
+			case Direction.RIGHT:
+				scrollOffset = new Vector3(scrollSpeed, 0);
+				break;
+			case Direction.UP:
+				scrollOffset = new Vector3(0,scrollSpeed);
+				break;
+			case Direction.DOWN:
+				scrollOffset = new Vector3(0,-scrollSpeed);
+				break;
+		}
+
+		foreach (GameObject tile in backgroundTiles)
+		{
+			tile.transform.position += scrollOffset;
+
+			switch (scrollDirection)
+			{
+				case Direction.LEFT:
+					if (tile.transform.position.x < -rt.rect.width / 2 + tileSize)
+					{
+						tile.transform.position = new Vector3(rt.rect.width / 2, tile.transform.position.y, tile.transform.position.z);
+					}
+					break;
+				case Direction.RIGHT:
+					if (tile.transform.position.x > rt.rect.width / 2 - tileSize)
+					{
+						tile.transform.position = new Vector3(-rt.rect.width / 2, tile.transform.position.y, tile.transform.position.z);
+					}
+					break;
+				case Direction.UP:
+					if (tile.transform.position.y > rt.rect.height / 2 - tileSize)
+					{
+						tile.transform.position = new Vector3(tile.transform.position.x, -rt.rect.height / 2, tile.transform.position.z);
+					}
+					break;
+				case Direction.DOWN:
+					if (tile.transform.position.y < -rt.rect.height / 2 + tileSize)
+					{
+						tile.transform.position = new Vector3(tile.transform.position.x, rt.rect.height / 2, tile.transform.position.z);
+					}
+					break;
+			}
+		}
+	}
+
+	void GenerateBackground()
+	{
+		float height = rt.rect.height;
+		float width = rt.rect.width;
+
+		int numVerticalTiles = (int)height / tileSize;
+		int numHorizontalTiles = (int)width / tileSize;
+
+		for (int j = 0; j < numVerticalTiles; j++)
+		{
+			for (int i = 0; i < numHorizontalTiles; i++)
+			{
+				GameObject go = Instantiate(tilePrefab) as GameObject;
+				Image image = go.GetComponent<Image>();
+				image.rectTransform.sizeDelta = new Vector2(tileSize, tileSize);
+				image.sprite = backgroundImages[Random.Range(0, backgroundImages.Count - 1)];
+				go.transform.position = new Vector3(i * tileSize - (width / 2 - tileSize/2), j * tileSize - (height / 2 - tileSize/2), 50);
+				go.transform.SetParent(this.transform);
+				backgroundTiles.Add(go);
+			}
+		}
+		
+	}
+	
+}
---
+++ Assets/_Scripts/Ship.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ship : MonoBehaviour {
+
+	[SerializeField]
+	float speed = 500f;
+	[SerializeField]
+	int health = 5;
+	[SerializeField]
+	float rotationLimit = 45f;
+
+	Rigidbody rb;
+
+
+	Vector3 CurRotation;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody>();
+		CurRotation = new Vector3(this.transform.rotation.x, this.transform.rotation.y, this.transform.rotation.z);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			CurRotation.y += speed;
+			rb.AddForce(Vector3.left * speed);
+			
+		} 
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			CurRotation.y -= speed;
+			rb.AddForce(Vector3.right * speed);
+		}
+
+		CurRotation.y = Mathf.Clamp(CurRotation.y, -rotationLimit, rotationLimit);
+		CurRotation.x = Mathf.Clamp(CurRotation.x, -rotationLimit, rotationLimit);
+		CurRotation.z = Mathf.Clamp(CurRotation.z, -rotationLimit, rotationLimit);
+
+		rb.transform.eulerAngles = CurRotation;
+	}
+}
